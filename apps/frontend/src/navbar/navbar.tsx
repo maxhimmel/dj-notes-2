02b45e15cd809f03617b5cdf8126b5effe7e766c
Max Himmel <himmelmax@gmail.com>
2025-02-25 10:09:22 -0800

    update navbar brand button to link to home page
    + (using react router!)

--- apps/frontend/src/navbar/navbar.tsx
+++ apps/frontend/src/navbar/navbar.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router";
+
 export default function Navbar() {
   return (
     <div className="navbar bg-base-300 text-base-content">
-      <button className="btn btn-ghost text-xl">Scratch Notes</button>
+      <Link to="/" className="btn btn-ghost text-xl">
+        Scratch Notes
+      </Link>
     </div>
   );
 }
