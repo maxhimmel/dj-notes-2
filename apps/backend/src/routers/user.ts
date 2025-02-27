5af53b5cc3649e4a6b1e9b143c36a335272a57d3
Max Himmel <himmelmax@gmail.com>
2025-02-26 16:39:30 -0800

    minor func param destructure change

--- apps/backend/src/routers/user.ts
+++ apps/backend/src/routers/user.ts
@@ -2,7 +2,7 @@ import { createRouter, publicProcedure } from "../lib/trpc";
 import { z } from "zod";
 
 export const userRouter = createRouter({
-  getUser: publicProcedure.input(z.string()).query((opts) => {
-    return { id: opts.input, name: "Bilbo" };
+  getUser: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return { id: input, name: "Bilbo" };
   }),
 });
