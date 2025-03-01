2c8f37a2b04add1c3873d820db30a0b10002f212
Max Himmel <himmelmax@gmail.com>
2025-03-01 15:40:57 -0800

    create signin and signout buttons
    + toggle which is shown based on session state

--- apps/frontend/src/navbar/navbar.tsx
+++ apps/frontend/src/navbar/navbar.tsx
@@ -1,11 +1,30 @@
+import { useSession } from "../auth/sessionProvider";
+import SignedIn from "../auth/signedIn";
+import SignedOut from "../auth/signedOut";
 import { Link } from "react-router";
 
 export default function Navbar() {
+  const { signIn, signOut } = useSession();
+
   return (
     <div className="navbar bg-base-300 text-base-content">
-      <Link to="/" className="btn btn-ghost text-xl">
-        Scratch Notes
-      </Link>
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          Scratch Notes
+        </Link>
+      </div>
+      <div className="flex-none">
+        <SignedOut>
+          <button onClick={signIn} className="btn btn-primary btn-outline">
+            Sign In
+          </button>
+        </SignedOut>
+        <SignedIn>
+          <button onClick={signOut} className="btn btn-primary">
+            Sign Out
+          </button>
+        </SignedIn>
+      </div>
     </div>
   );
 }
