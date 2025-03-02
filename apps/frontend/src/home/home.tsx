467e3230bca33b82b69ec74ecd9f4b670c376c99
Max Himmel <himmelmax@gmail.com>
2025-03-01 16:03:09 -0800

    small test

--- apps/frontend/src/home/home.tsx
+++ apps/frontend/src/home/home.tsx
@@ -1,3 +1,22 @@
+import { trpc } from "@trpc/frontend";
+import { useSession } from "../auth/sessionProvider";
+
 export default function Home() {
-  return <>Howdy world!</>;
+  const { session } = useSession();
+  const getUser = trpc.user.getUser.useQuery("Hello world!");
+
+  return (
+    <ul className="flex flex-col">
+      <li>
+        User's name? {"-->"} {session?.user?.name ?? "Signed out"}
+      </li>
+      <li>
+        {"API Test: /api/user.getUser?input='Hello world!' --> < " +
+          getUser.data?.id +
+          ", " +
+          getUser.data?.name +
+          " >"}
+      </li>
+    </ul>
+  );
 }
