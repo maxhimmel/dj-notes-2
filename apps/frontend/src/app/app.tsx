07afd17d11ca771a50411dfa33e15f16314fd74f
Max Himmel <himmelmax@gmail.com>
2025-03-03 16:52:24 -0800

    scaffold set list collection page/route

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -3,6 +3,7 @@ import SessionProvider from "../auth/sessionProvider";
 import Home from "../home/home";
 import Navbar from "../navbar/navbar";
 import TrpcProvider from "../trpc";
+import SetListCollection from "../setLists/setListCollection";
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
           <Navbar />
           <Routes>
             <Route index element={<Home />}></Route>
+            <Route path="/setlists" element={<SetListCollection />}></Route>
           </Routes>
         </TrpcProvider>
       </SessionProvider>
