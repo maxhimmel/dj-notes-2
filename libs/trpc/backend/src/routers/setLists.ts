30ca3c4a3ab40278eae2e622f1d87bc4590bd3d2
Max Himmel <himmelmax@gmail.com>
2025-03-05 12:54:14 -0800

    setlists api: create, index, delete

--- libs/trpc/backend/src/routers/setLists.ts
+++ libs/trpc/backend/src/routers/setLists.ts
@@ -2,12 +2,27 @@ import { z } from "zod";
 import { createRouter, protectedProcedure } from "../trpc";
 
 export const setListRouter = createRouter({
-  create: protectedProcedure.mutation(({ ctx, input }) => {
-    return { id: input, name: "Bilbo" };
-  }),
+  create: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+      const newSetList = await ctx.db.setList.create({
+        data: {
+          name: input.name,
+          userId: user.id as string,
+        },
+      });
 
-  getSets: protectedProcedure.query(({ ctx, input }) => {
-    return { id: input, name: "Bilbo" };
+      return { setList: newSetList };
+    }),
+
+  getSets: protectedProcedure.query(async ({ ctx }) => {
+    const user = ctx.session.user;
+    const setLists = await ctx.db.setList.findMany({
+      where: { userId: user.id },
+    });
+
+    return { setLists };
   }),
 
   getSetList: protectedProcedure
@@ -18,8 +33,13 @@ export const setListRouter = createRouter({
 
   delete: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .mutation(({ ctx, input }) => {
-      return { id: input, name: "Bilbo" };
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+      const deletedSetList = await ctx.db.setList.delete({
+        where: { id: input.id, userId: user.id },
+      });
+
+      return { setList: deletedSetList };
     }),
 
   update: protectedProcedure
