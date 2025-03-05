4eedbc2de592c00f5aa8e8d50e57a10c1e89f777
Max Himmel <himmelmax@gmail.com>
2025-03-05 10:43:35 -0800

    add prisma adapter to auth.js

--- libs/trpc/backend/src/routers/auth.ts
+++ libs/trpc/backend/src/routers/auth.ts
@@ -1,10 +1,15 @@
 import { ExpressAuth, getSession as getAuthSession } from "@auth/express";
 import Google from "@auth/express/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@dj-notes-2/shared";
 import { Request } from "express";
 
 const PROVIDERS = [Google];
 
-export const authHandler = ExpressAuth({ providers: PROVIDERS });
+export const authHandler = ExpressAuth({
+  providers: PROVIDERS,
+  adapter: PrismaAdapter(prisma),
+});
 
 export function getSession(req: Request) {
   return getAuthSession(req, { providers: PROVIDERS });
