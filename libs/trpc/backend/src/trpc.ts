85d03133adad55426721124f8e6ca7687250c9d5
Max Himmel <himmelmax@gmail.com>
2025-03-05 15:21:01 -0800

    hook up superjson transformer for trpc

--- libs/trpc/backend/src/trpc.ts
+++ libs/trpc/backend/src/trpc.ts
@@ -2,6 +2,7 @@ import { initTRPC, TRPCError } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { getSession } from "./routers/auth";
 import { prisma } from "@dj-notes-2/shared";
+import superjson from "superjson";
 
 export async function createContext({
   req,
@@ -17,7 +18,9 @@ export async function createContext({
   };
 }
 type Context = Awaited<ReturnType<typeof createContext>>;
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+});
 
 export const createRouter = t.router;
 
