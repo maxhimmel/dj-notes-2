c1c1531a43ac32a04f5ac51c7097ed67ea88ca2e
Max Himmel <himmelmax@gmail.com>
2025-03-21 15:05:45 -0700

    take advantage of navigate > state

--- apps/frontend/src/setLists/setListCollection.tsx
+++ apps/frontend/src/setLists/setListCollection.tsx
@@ -7,7 +7,6 @@ import {
   RiSparkling2Fill,
 } from "react-icons/ri";
 import { useNavigate } from "react-router";
-import { useSetList } from "./setListProvider";
 
 export default function SetListCollection() {
   const getSets = trpc.setLists.getSets.useQuery();
@@ -62,7 +61,6 @@ export default function SetListCollection() {
 
 function NewSetListButton() {
   const navigate = useNavigate();
-  const { initializeSetList } = useSetList();
   const createSetList = trpc.setLists.create.useMutation();
 
   async function handleNewSetList() {
@@ -70,8 +68,7 @@ function NewSetListButton() {
       name: "New Set List",
     });
 
-    initializeSetList(setList);
-    navigate(`/setlists/${setList.id}`);
+    navigate(`/setlists/${setList.id}`, { state: setList });
   }
   return (
     <button onClick={handleNewSetList} className="btn btn-secondary">
@@ -104,11 +101,9 @@ function SetListEntry(props: {
   deleteSet: (id: string) => Promise<void>;
 }) {
   const navigate = useNavigate();
-  const { initializeSetList } = useSetList();
 
   function handleEdit() {
-    initializeSetList(props.entry);
-    navigate(`/setlists/${props.entry.id}`);
+    navigate(`/setlists/${props.entry.id}`, { state: props.entry });
   }
 
   async function handleDelete(evt: React.MouseEvent) {
